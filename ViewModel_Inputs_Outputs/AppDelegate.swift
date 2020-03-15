d0ca7fef8efeaa44a12926316693d2a5a2456b7c
Raul Mena <>
2020-03-15 01:02:59 -0400

    MVVM with Inputs/Outputs protocols

---
+++ ViewModel_Inputs_Outputs/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  ViewModel_Inputs_Outputs
+//
+//  Created by Raul Mena on 3/14/20.
+//  Copyright © 2020 Raul Mena. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+
+        return true
+    }
+
+}
+
